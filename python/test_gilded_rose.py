cffaa293c42e9bb8ac299326ebcb96c188683ac3
Nicholas Walton <nicholas.walton@gmail.com>
2024-09-13 13:24:11 -0700

    python: fixture-ize tests

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -4,59 +4,62 @@ import pytest
 from gilded_rose import Item, GildedRose
 
 
-def create_and_update_rose(name, sell_in, quality):
+@pytest.fixture
+def item(request, sell_in, quality):
+    name = request.node.get_closest_marker("item_name").args[0]
     items = [Item(name, sell_in, quality)]
     gilded_rose = GildedRose(items)
     gilded_rose.update_quality()
-    return items
+    return items[0]
 
 
-@pytest.mark.parametrize("name", ("foo",))
-def test_foo(name):
-    items = create_and_update_rose(name, 0, 0)
-    assert name == items[0].name
+@pytest.mark.item_name("foo")
+@pytest.mark.parametrize("sell_in,quality", ((0, 0),))
+def test_foo(item):
+    assert "foo" == item.name
 
 
+@pytest.mark.item_name("Sausage")
 @pytest.mark.parametrize(
     "sell_in,quality,expected_quality",
     ((5, 10, 9), (1, 10, 9), (0, 10, 8), (-5, 20, 18), (10, 0, 0)),
 )
-def test_generic_item_ages(sell_in, quality, expected_quality):
-    items = create_and_update_rose("Sausage", sell_in, quality)
-    assert sell_in - 1 == items[0].sell_in
-    assert expected_quality == items[0].quality
+def test_generic_item_ages(item, sell_in, expected_quality):
+    assert sell_in - 1 == item.sell_in
+    assert expected_quality == item.quality
 
 
+@pytest.mark.item_name("Conjured Sausage")
 @pytest.mark.parametrize(
     "sell_in,quality,expected_quality",
     ((5, 10, 8), (1, 10, 8), (0, 10, 6), (-5, 20, 16), (10, 0, 0)),
 )
-def test_generic_conjured_item_ages(sell_in, quality, expected_quality):
-    items = create_and_update_rose("Conjured Sausage", sell_in, quality)
-    assert sell_in - 1 == items[0].sell_in
-    assert expected_quality == items[0].quality
+def test_generic_conjured_item_ages(item, sell_in, expected_quality):
+    assert sell_in - 1 == item.sell_in
+    assert expected_quality == item.quality
 
 
+@pytest.mark.item_name("Aged Brie")
 @pytest.mark.parametrize(
     "sell_in,quality,expected_quality",
     ((5, 10, 11), (1, 10, 11), (0, 10, 12), (-5, 20, 22), (10, 50, 50)),
 )
-def test_aged_brie_ages(sell_in, quality, expected_quality):
-    items = create_and_update_rose("Aged Brie", sell_in, quality)
-    assert sell_in - 1 == items[0].sell_in
-    assert expected_quality == items[0].quality
+def test_aged_brie_ages(item, sell_in, expected_quality):
+    assert sell_in - 1 == item.sell_in
+    assert expected_quality == item.quality
 
 
+@pytest.mark.item_name("Sulfuras, Hand of Ragnaros")
 @pytest.mark.parametrize(
-    "sell_in,quality,expected_quality",
-    ((5, 10, 10), (1, 10, 10), (0, 10, 10), (-5, 20, 20), (5, 80, 80)),
+    "sell_in,quality",
+    ((5, 10), (1, 10), (0, 10), (-5, 20), (5, 80)),
 )
-def test_sulfuras_ages(sell_in, quality, expected_quality):
-    items = create_and_update_rose("Sulfuras, Hand of Ragnaros", sell_in, quality)
-    assert sell_in == items[0].sell_in
-    assert expected_quality == items[0].quality
+def test_sulfuras_ages(item, sell_in, quality):
+    assert sell_in == item.sell_in
+    assert quality == item.quality
 
 
+@pytest.mark.item_name("Backstage passes to a TAFKAL80ETC concert")
 @pytest.mark.parametrize(
     "sell_in,quality,expected_quality",
     (
@@ -70,9 +73,6 @@ def test_sulfuras_ages(sell_in, quality, expected_quality):
         (5, 50, 50),
     ),
 )
-def test_backstage_ages(sell_in, quality, expected_quality):
-    items = create_and_update_rose(
-        "Backstage passes to a TAFKAL80ETC concert", sell_in, quality
-    )
-    assert sell_in - 1 == items[0].sell_in
-    assert expected_quality == items[0].quality
+def test_backstage_ages(item, sell_in, expected_quality):
+    assert sell_in - 1 == item.sell_in
+    assert expected_quality == item.quality
