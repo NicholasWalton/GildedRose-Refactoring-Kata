21ed4a8fe3b06383c6df0694aed1ace0132db56a
Nicholas Walton <nicholas.walton@gmail.com>
2024-09-13 09:23:54 -0700

    python: write _item_quality_modifier()

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -14,34 +14,32 @@ class GildedRose(object):
         for item in self.items:
             self._update_item_quality(item)
 
+    def _item_quality_modifier(self, quality, modifier):
+        intermediate = min(MAX_QUALITY, quality + modifier)
+        return max(MINIMUM_QUALITY, intermediate)
+
     def _update_item_quality(self, item):
         if item.name != AGED_BRIE and item.name != BACKSTAGE_PASSES:
-            if item.quality > MINIMUM_QUALITY:
-                if item.name != SULFURAS:
-                    item.quality = item.quality - 1
+            if item.name != SULFURAS:
+                item.quality = self._item_quality_modifier(item.quality, -1)
         else:
-            if item.quality < MAX_QUALITY:
-                item.quality = item.quality + 1
-                if item.name == BACKSTAGE_PASSES:
-                    if item.sell_in < 11:
-                        if item.quality < MAX_QUALITY:
-                            item.quality = item.quality + 1
-                    if item.sell_in < 6:
-                        if item.quality < MAX_QUALITY:
-                            item.quality = item.quality + 1
+            item.quality = self._item_quality_modifier(item.quality, 1)
+            if item.name == BACKSTAGE_PASSES:
+                if item.sell_in < 11:
+                    item.quality = self._item_quality_modifier(item.quality, 1)
+                if item.sell_in < 6:
+                    item.quality = self._item_quality_modifier(item.quality, 1)
         if item.name != SULFURAS:
             item.sell_in = item.sell_in - 1
         if item.sell_in < 0:
             if item.name != AGED_BRIE:
                 if item.name != BACKSTAGE_PASSES:
-                    if item.quality > MINIMUM_QUALITY:
-                        if item.name != SULFURAS:
-                            item.quality = item.quality - 1
+                    if item.name != SULFURAS:
+                        item.quality = self._item_quality_modifier(item.quality, -1)
                 else:
-                    item.quality = item.quality - item.quality
+                    item.quality = MINIMUM_QUALITY
             else:
-                if item.quality < MAX_QUALITY:
-                    item.quality = item.quality + 1
+                item.quality = self._item_quality_modifier(item.quality, 1)
 
 
 class Item:
