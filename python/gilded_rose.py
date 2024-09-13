38e054f9a96eecdeb40cb14ad47ae7e409875aa1
Nicholas Walton <nicholas.walton@gmail.com>
2024-09-13 09:16:30 -0700

    python: test coverage, basic Extracts

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,39 +1,47 @@
 # -*- coding: utf-8 -*-
+MINIMUM_QUALITY = 0
+BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
+AGED_BRIE = "Aged Brie"
+SULFURAS = "Sulfuras, Hand of Ragnaros"
+MAX_QUALITY = 50
+
 
 class GildedRose(object):
-
     def __init__(self, items):
         self.items = items
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
+            self._update_item_quality(item)
+
+    def _update_item_quality(self, item):
+        if item.name != AGED_BRIE and item.name != BACKSTAGE_PASSES:
+            if item.quality > MINIMUM_QUALITY:
+                if item.name != SULFURAS:
+                    item.quality = item.quality - 1
+        else:
+            if item.quality < MAX_QUALITY:
+                item.quality = item.quality + 1
+                if item.name == BACKSTAGE_PASSES:
+                    if item.sell_in < 11:
+                        if item.quality < MAX_QUALITY:
+                            item.quality = item.quality + 1
+                    if item.sell_in < 6:
+                        if item.quality < MAX_QUALITY:
+                            item.quality = item.quality + 1
+        if item.name != SULFURAS:
+            item.sell_in = item.sell_in - 1
+        if item.sell_in < 0:
+            if item.name != AGED_BRIE:
+                if item.name != BACKSTAGE_PASSES:
+                    if item.quality > MINIMUM_QUALITY:
+                        if item.name != SULFURAS:
+                            item.quality = item.quality - 1
                 else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                    item.quality = item.quality - item.quality
+            else:
+                if item.quality < MAX_QUALITY:
+                    item.quality = item.quality + 1
 
 
 class Item:
--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -1,16 +1,68 @@
 # -*- coding: utf-8 -*-
-import unittest
+import pytest
 
 from gilded_rose import Item, GildedRose
 
 
-class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEqual("fixme", items[0].name)
+def create_and_update_rose(name, sell_in, quality):
+    items = [Item(name, sell_in, quality)]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+    return items
 
-        
-if __name__ == '__main__':
-    unittest.main()
+
+@pytest.mark.parametrize("name", ("foo",))
+def test_foo(name):
+    items = create_and_update_rose(name, 0, 0)
+    assert name == items[0].name
+
+
+@pytest.mark.parametrize(
+    "sell_in,quality,expected_quality",
+    ((5, 10, 9), (1, 10, 9), (0, 10, 8), (-5, 20, 18), (10, 0, 0)),
+)
+def test_generic_item_ages(sell_in, quality, expected_quality):
+    items = create_and_update_rose("Sausage", sell_in, quality)
+    assert sell_in - 1 == items[0].sell_in
+    assert expected_quality == items[0].quality
+
+
+@pytest.mark.parametrize(
+    "sell_in,quality,expected_quality",
+    ((5, 10, 11), (1, 10, 11), (0, 10, 12), (-5, 20, 22), (10, 50, 50)),
+)
+def test_aged_brie_ages(sell_in, quality, expected_quality):
+    items = create_and_update_rose("Aged Brie", sell_in, quality)
+    assert sell_in - 1 == items[0].sell_in
+    assert expected_quality == items[0].quality
+
+
+@pytest.mark.parametrize(
+    "sell_in,quality,expected_quality",
+    ((5, 10, 10), (1, 10, 10), (0, 10, 10), (-5, 20, 20)),
+)
+def test_sulfuras_ages(sell_in, quality, expected_quality):
+    items = create_and_update_rose("Sulfuras, Hand of Ragnaros", sell_in, quality)
+    assert sell_in == items[0].sell_in
+    assert expected_quality == items[0].quality
+
+
+@pytest.mark.parametrize(
+    "sell_in,quality,expected_quality",
+    (
+        (11, 10, 11),
+        (9, 10, 12),
+        (5, 10, 13),
+        (1, 10, 13),
+        (0, 10, 0),
+        (-5, 20, 0),
+        (5, 49, 50),
+        (5, 50, 50),
+    ),
+)
+def test_backstage_ages(sell_in, quality, expected_quality):
+    items = create_and_update_rose(
+        "Backstage passes to a TAFKAL80ETC concert", sell_in, quality
+    )
+    assert sell_in - 1 == items[0].sell_in
+    assert expected_quality == items[0].quality
